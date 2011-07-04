73c62766defe2fcb38e32a4a09803aea5327f1be
Nicolas Brassard <nico@nitriques.com>
2011-07-04 16:11:52 -0400

    Added French version

---
+++ lang/lang.fr.php
@@ -0,0 +1,44 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Solutions Nitriques',
+			'email' => 'open-source (at) nitriques.com',
+		),
+		'release-date' => '2011-07-04'
+	);
+
+	/**
+	 * Events, Datasources & Utilities Indexes
+	 */
+	$dictionary = array(
+
+		'Filters' => 
+		'Filtre',
+
+		'Not found' => 
+		'Non-trouvé',
+
+		'Link Page' => 
+		'Lier Page',
+
+		'Unlink Page' => 
+		'Ne plus lier page',
+
+		'All' => 
+		'Tous',
+
+		'equals' => 
+		'équal',
+
+		'contains' => 
+		'contient',
+
+		'is empty' => 
+		'est vide',
+
+		'Remove filter' => 
+		'Supprimer filtre',
+
+	);
