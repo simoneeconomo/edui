ceb423abef4f9098c423638e59f91defc5d917b0
Nicolas Brassard <nico@nitriques.com>
2011-07-18 20:39:05 -0400

    Fix a bug where you cannot unlink to a page if the ressource is the last one. Closes issue #10

--- lib/class.pagemanager.php
+++ lib/class.pagemanager.php
@@ -23,12 +23,12 @@
 				$result = $results[0][$field];
 
 				if (!in_array($r_handle, explode(',', $result))) {
-					
+
 					// add only if requiered
 					if (strlen($result) > 0) {
 						$result .= ",";
 					}
-					
+
 					// append new ressource
 					$result .= $r_handle;
 
@@ -50,22 +50,22 @@
 
 			if (is_array($results) && count($results) == 1) {
 				$result = $results[0][$field];
-				
-				$ex_result = explode(',', $result);
-				
-				$idx = array_search($r_handle, $ex_result, false);
 
-				if ($idx != FALSE) {
-					
-					// Do not use str_replace since this may replace some good infos.
-					// ex.: When unlinking DS videos, another DS named video_2
-					//      will be transform into _2, which breaks the front end
-					//$result = str_replace($r_handle, '', $result);
-					//$result = str_replace(',,', ',', $result);
-					
+				// Do not use str_replace since this may replace some good infos.
+				// ex.: When unlinking DS videos, another DS named video_2
+				//      will be transform into _2, which breaks the front end
+				//$result = str_replace($r_handle, '', $result);
+				//$result = str_replace(',,', ',', $result);
+
+				$ex_result = explode(',', $result);
+
+				$idx = array_search($r_handle, $ex_result, true);
+
+				if ($idx !== FALSE) { // I got fooled again by 0 == FALSE but 0 !== FALSE
+
 					// remove the element from the array
 					array_splice($ex_result, $idx, 1);
-					
+
 					// get the string back
 					$result = implode(',', $ex_result);
 
@@ -93,8 +93,8 @@
 		public function unlinkEvent($e_handle, $page_id) {
 			$this->unlinkResource("events", $e_handle, $page_id);
 		}
-		
-		
+
+
 		/**
 		 *
 		 * Appends a new field, 'path' in each array in $flat and appends the parents page title to the current one
@@ -118,7 +118,7 @@
 					if (strlen($handle) > 0) {
 						$path = $handle . '/' . $path;
 					}
-					
+
 					// new title
 					$title = $flat[$j]->title . ' - ' . $title;
 				}
@@ -138,7 +138,7 @@
 				$flat[$i]->title = trim($title, ' - ');
 			}
 		}
-		
+
 		/**
 		 *
 		 * Generate a "flat" view of the current page and ancestors
@@ -153,52 +153,52 @@
 				// modify SQL query
 				$cols = "id, parent, page_lhandles_t_$lg as title, page_lhandles_h_$lg as handle";
 			}
-			
+
 			$pages = array();
-			
+
 			// get all pages
 			if (Symphony::Database()->query("SELECT $cols FROM `tbl_pages`")) {
 				$pages = Symphony::Database()->fetch();
 			}
-			
+
 			foreach ($pages as $page) {
-				
+
 				$pageTree = array();
-				
+
 				array_push($pageTree, $page);
-			
+
 				// try get all parents
 				$cid = (int) $page->parent;
-				
+
 				// while we still find parents
 				while ($cid > 0) {
-					
+
 					$pid = $cid;
-					
+
 					// search for prent in array
 					for ($i = 0; $i < count($pages) && $pid > 0; $i++) {
 						if ($pages[$i]->id == $cid) {
-							
+
 							array_push($pageTree, $pages[$i]);
-							
+
 							$cid = $subPage->parent;
 							$pid = -1;
 						}
 					}
 				}
-				
+
 				$this->buildPathAndTitle($pageTree);
-				
+
 				// add page in array
 				array_push($flat, get_object_vars($pageTree[0]));
 
 			}
-			
+
 			usort($flat, array( $this, 'compareTitles' ) );
 
 			return $flat;
 		}
-		
+
 		protected function compareTitles($a, $b) {
 			return strcasecmp ($a['title'], $b['title']);
 		}
